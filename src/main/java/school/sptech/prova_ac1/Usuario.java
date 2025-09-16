38196c806cfec8b3d654df442d92d4cab4e56103
GabrielSousaSPTech <gabriel.sousa@sptech.school>
2025-09-16 19:47:39 -0300

    Inicio da atividade

--- src/main/java/school/sptech/prova_ac1/Usuario.java
+++ src/main/java/school/sptech/prova_ac1/Usuario.java
@@ -1,5 +1,53 @@
 package school.sptech.prova_ac1;
 
+import java.time.LocalDate;
+
+
 public class Usuario {
-    
+
+    private Integer id;
+    private String nome;
+    private String cpf;
+    private String senha;
+    private LocalDate dataNascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
